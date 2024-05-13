b75a5cedb296f8feb48b6c0881ff267a3f54095e
Soorya003 <51847100+Soorya003@users.noreply.github.com>
2024-05-13 20:44:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function foo(){
+    var res = document.getElementById("fn").value;
+    console.log(res);
+    var res1 = document.getElementById("mn").value;
+    console.log(res1);
+    var res2 = document.getElementById("ln").value;
+    console.log(res2);
+    var res3 = document.getElementById("em").value;
+    console.log(res3);
+    var res4 = document.getElementById("lo").value;
+    console.log(res4);
+}
